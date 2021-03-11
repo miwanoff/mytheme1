fe9470b6c6114bde3cf7dd17e75dc15e3f51d7b3
miwanoff <marina.iva.noffa@gmail.com>
2021-03-11 17:13:55 +0200

    add Load More Button Plugin

---
+++ wp-content/plugins/load-more-button/load-more-button.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Load More Button
+Plugin URI: http://ex.com/
+Description: Simple Load More Button plugin with AJAX
+Author: MAI
+Version: 1.1
+Author URI: http://ex.com/
+ */

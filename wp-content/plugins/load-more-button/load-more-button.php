e09704b5ec0fe23c881afba31ccaeb6a684365ea
miwanoff <marina.iva.noffa@gmail.com>
2021-03-11 17:32:41 +0200

    add wp_register_scripts

--- wp-content/plugins/load-more-button/load-more-button.php
+++ wp-content/plugins/load-more-button/load-more-button.php
@@ -7,6 +7,19 @@ Author: MAI
 Version: 1.1
 Author URI: http://ex.com/
  */
+
+function bootkit_load_more_scripts()
+{
+    // In most cases it is already included on the page and this line can be removed
+    wp_enqueue_script('jquery');
+
+    // register our main script but do not enqueue it yet
+    wp_register_script('loadmore', plugin_dir_url(__FILE__) . 'js/loadmore.js', array('jquery'));
+    wp_enqueue_script('loadmore');
+}
+
+add_action('wp_enqueue_scripts', 'bootkit_load_more_scripts');
+
 function create_button_and_script()
 {
 

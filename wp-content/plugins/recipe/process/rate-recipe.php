40dc69b21b675a7afb070cc156b73256ec131b82
miwanoff <marina.iva.noffa@gmail.com>
2021-03-18 18:24:40 +0200

    change r_rate_recipe()

--- wp-content/plugins/recipe/process/rate-recipe.php
+++ wp-content/plugins/recipe/process/rate-recipe.php
@@ -1,5 +1,23 @@
 <?php
 function r_rate_recipe()
 {
-    print_r($_POST);
+    global $wpdb;
+
+    $output = ['status' => 1];
+    $post_ID = absint($_POST['rid']);
+    $rating = round($_POST['rating'], 1);
+    $user_IP = $_SERVER['REMOTE_ADDR'];
+
+    // Insert Rating into database
+    $wpdb->insert(
+        $wpdb->prefix . 'recipe_ratings',
+        [
+            'recipe_id' => $post_ID,
+            'rating' => $rating,
+            'user_ip' => $user_IP,
+        ],
+        ['%d', '%f', '%s']
+    );
+    $output['status'] = 2;
+    wp_send_json($output);
 }

83980f9344bf37de3b15c028ef7c54ef71f365c4
miwanoff <marina.iva.noffa@gmail.com>
2021-03-18 17:57:01 +0200

    add r_enqueue_scripts()

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -15,17 +15,20 @@ if (!function_exists('add_action')) {
 }
 
 // Setup
+define('RECIPE_PLUGIN_URL', __FILE__);
 
 // Includes
 include 'includes/activate.php';
 include 'includes/init.php';
 include 'process/save-post.php';
 include 'process/filter-content.php';
+include 'includes/front/enqueue.php';
 
 // Hooks
 register_activation_hook(__FILE__, 'r_activate_plugin');
 add_action('init', 'recipe_init');
 add_action('save_post_recipe', 'r_save_post_admin', 10, 3);
 add_filter('the_content', 'r_filter_recipe_content');
+add_action('wp_enqueue_scripts', 'r_enqueue_scripts', 100);
 
 // Shortcodes

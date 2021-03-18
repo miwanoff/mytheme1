1ced5e0b53d0b43e8e81ef474fcf3ed33d1b3085
miwanoff <marina.iva.noffa@gmail.com>
2021-03-18 22:21:11 +0200

    fix loadmore for singular

--- wp-content/plugins/load-more-scroll/load-more-scroll.php
+++ wp-content/plugins/load-more-scroll/load-more-scroll.php
@@ -10,12 +10,14 @@ Author URI: http://ex.com/
 
 function bootkit_load_more_scripts()
 {
-    // In most cases it is already included on the page and this line can be removed
-    wp_enqueue_script('jquery');
+    if (!is_singular()) {
+        // In most cases it is already included on the page and this line can be removed
+        wp_enqueue_script('jquery');
 
-    // register our main script but do not enqueue it yet
-    wp_register_script('loadmore', plugin_dir_url(__FILE__) . 'js/loadmore.js', array('jquery'));
-    wp_enqueue_script('loadmore');
+        // register our main script but do not enqueue it yet
+        wp_register_script('loadmore', plugin_dir_url(__FILE__) . 'js/loadmore.js', array('jquery'));
+        wp_enqueue_script('loadmore');
+    }
 }
 
 add_action('wp_enqueue_scripts', 'bootkit_load_more_scripts');
@@ -37,6 +39,7 @@ function create_script()
     }
 
 }
+
 add_action('get_footer', 'create_script');
 
 function bootkit_load_posts()

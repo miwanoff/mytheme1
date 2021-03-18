5fd87f3c8ce935dcb96f08679bc17e102d7aede1
miwanoff <marina.iva.noffa@gmail.com>
2021-03-18 18:19:24 +0200

    add AJAX

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -23,6 +23,7 @@ include 'includes/init.php';
 include 'process/save-post.php';
 include 'process/filter-content.php';
 include 'includes/front/enqueue.php';
+include 'process/rate-recipe.php';
 
 // Hooks
 register_activation_hook(__FILE__, 'r_activate_plugin');
@@ -30,5 +31,7 @@ add_action('init', 'recipe_init');
 add_action('save_post_recipe', 'r_save_post_admin', 10, 3);
 add_filter('the_content', 'r_filter_recipe_content');
 add_action('wp_enqueue_scripts', 'r_enqueue_scripts', 100);
+add_action('wp_ajax_r_rate_recipe', 'r_rate_recipe');
+add_action('wp_ajax_nopriv_r_rate_recipe', 'r_rate_recipe');
 
 // Shortcodes

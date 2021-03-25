f582d32c36c8830dd883910aee02ab76fcaeae90
miwanoff <marina.iva.noffa@gmail.com>
2021-03-25 18:00:12 +0200

    change function widget

--- wp-content/plugins/recipe/includes/widgets/daily-recipe.php
+++ wp-content/plugins/recipe/includes/widgets/daily-recipe.php
@@ -60,6 +60,41 @@ class R_Daily_Recipe_Widget extends WP_Widget
      */
     public function widget($args, $instance)
     {
-        echo 'recipe of the day';
+        // echo 'recipe of the day';
+        extract($args);
+        extract($instance);
+
+        $title = apply_filters('widget_title', $title);
+
+        echo $before_widget;
+        echo $before_title . $title . $after_title;
+
+        $recipe_id = get_transient('r_daily_recipe');
+
+        if (!$recipe_id) {
+            $recipe_id = r_get_random_recipe();
+
+            set_transient(
+                'r_daily_recipe',
+                $recipe_id,
+                DAY_IN_SECONDS
+            );
+        }
+
+        ?>
+<div class="recepi-image">
+    <a href="<?php echo get_permalink($recipe_id); ?>">
+        <?php echo get_the_post_thumbnail($recipe_id, 'thumbnail'); ?>
+    </a>
+</div>
+<div class="recepi-desc center nobottompadding">
+    <h4 class="mb-4"><a href="<?php echo get_permalink($recipe_id); ?>">
+            <?php echo get_the_title($recipe_id); ?>
+        </a></h3>
+</div>
+<?php
+
+        echo $after_widget;
+
     }
 }

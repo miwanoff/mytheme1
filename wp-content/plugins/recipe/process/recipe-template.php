ede198ba7b5bab8fc3a8ce91fc2041348b4aba64
miwanoff <marina.iva.noffa@gmail.com>
2021-03-25 16:53:40 +0200

    show RECIPE_RATING

--- wp-content/plugins/recipe/process/recipe-template.php
+++ wp-content/plugins/recipe/process/recipe-template.php
@@ -2,6 +2,6 @@
     <li>
         <strong>RATE_I18N: </strong>
         <div id="recipe_rating" class="rateit" READONLY_PLACEHOLDER data-rateit-value="RECIPE_RATING"
-            data-rateit-resetable="false" data-rid="RECIPE_ID"></div>
+            data-rateit-resetable="false" data-rid="RECIPE_ID"></div> <span class="rr">RECIPE_RATING</span>
     </li>
 </ul>

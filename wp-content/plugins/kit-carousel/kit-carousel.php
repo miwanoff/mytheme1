35ced9211297c22615e3d8384f006a3dd2fafe90
miwanoff <marina.iva.noffa@gmail.com>
2021-04-01 18:01:09 +0300

    fill kit-carousel.php

---
+++ wp-content/plugins/kit-carousel/kit-carousel.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Plugin Name: Kit Carousel
+ * Plugin URI:
+ * Description: Simple carousel
+ * Version: 1.0
+ * Author: MAI
+ * Author URI: https://github.com/miwanoff/
+ * License:           GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+// Setup
+
+// Includes
+
+// Hooks
+
+// Shortcodes
